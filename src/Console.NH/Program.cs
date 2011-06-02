7d476968d2cda0390c80657fb61c20a6801110a8
Philip Proplesch <philip@proplesch.de>
2011-06-02 20:43:56 +0200

    Added a first sample application.

---
+++ src/Console.NH/Program.cs
@@ -0,0 +1,28 @@
+﻿using Console.NH.Models;
+using Microsoft.Practices.ServiceLocation;
+using Uranium.Core.Data;
+using Uranium.Core.Infrastructure;
+
+namespace Console.NH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bootstrapper.Bootstrap(
+                asm => asm.FullName.StartsWith("Console."));
+
+            var repository =
+                ServiceLocator.Current.GetInstance<IRepository<User>>();
+
+            var user =
+                new User
+                    {
+                        FirstName = "Philip",
+                        LastName = "Proplesch"
+                    };
+
+            repository.Insert(user);
+        }
+    }
+}
